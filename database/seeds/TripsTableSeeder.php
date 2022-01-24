7a014de21c6f2ab6fbf21dff1bfb9de5e03a064d
DanyMencu <dany69men@gmail.com>
2022-01-24 14:48:57 +0100

    Create model Trip and relative seeder

---
+++ database/seeds/TripsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+use App\Trip;
+
+class TripsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $new_trip = new Trip;
+
+        
+    }
+}
